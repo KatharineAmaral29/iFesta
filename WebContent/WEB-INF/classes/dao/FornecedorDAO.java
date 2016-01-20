c8eced4fa11b2768039f9c9160975400c5c1792f
LucasFernandes28 <lucas.andrade@alu.ufc.br>
2016-01-20 18:05:23 -0300

    Atualização de DAO

--- WebContent/WEB-INF/classes/dao/FornecedorDAO.java
+++ WebContent/WEB-INF/classes/dao/FornecedorDAO.java
@@ -1,8 +1,6 @@
 
 public class FornecedorDAO {
 
-	public FornecedorDAO() {
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
--- WebContent/WEB-INF/classes/dao/ServicoDAO.java
+++ WebContent/WEB-INF/classes/dao/ServicoDAO.java
@@ -1,8 +1,6 @@
 
 public class ServicoDAO {
 
-	public ServicoDAO() {
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
