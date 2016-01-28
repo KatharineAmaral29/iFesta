d80536b0b42dd770590f2ba188fb34b8168328f0
Katharine Amaral <katharineamaral@alu.ufc.br>
2016-01-28 20:52:58 -0200

    Atualização 28/01

--- WebContent/WEB-INF/CadastroClienteServlet.java
+++ WebContent/WEB-INF/CadastroClienteServlet.java
@@ -1,4 +1,11 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class CadastroClienteServlet {
+@WebServlet("/CadastroClienteServlet")
+public class CadastroClienteServlet extends HttpServlet {
 
 }
