95c5573666df103fc3a4ca2353a01620839c2544
Katharine Amaral <katharineamaral@alu.ufc.br>
2016-02-02 21:36:29 -0200

    servlets atualizacao

--- WebContent/WEB-INF/classes/servlet/LoginFornecedorServlet.java
+++ WebContent/WEB-INF/classes/servlet/LoginFornecedorServlet.java
@@ -11,5 +11,19 @@ import servlet.*;
 
 @WebServlet("/LoginFornecedorServlet")
 public class LoginFornecedorServlet  extends HttpServlet {
+	public String login = "KatharineFornecedor";
+	public String senha = "12345";
 
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {	
+		String login_form = request.getParameter("emailf"); // Pega o Login vindo do formulário
+		String senha_form = request.getParameter("senhaf"); //Pega a senha vinda do formulário
+		if(login_form.equals(login) && senha_form.equals(senha)){ //Caso login e senha estejam corretos...
+			out.println("Logado com sucesso."); //Mostra na tela que foi logado com sucesso
+			session.putValue("loginUsuario", login); //Grava a session com o Login
+			session.putValue("senhaUsuario", senha); //Grava a session com a Senha
+			out.println("<script>document.location.href='logado.jsp';</script>"); //Exibe um código javascript para redireionar ao painel
+		}
+		else{ //Se estiverem incorretos...
+			out.println("Login ou senha inválidos. <a href='java script:back()'>Voltar</a>"); //Exibe na tela e pede para voltar
+		}
 }
