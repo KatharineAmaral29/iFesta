5f54707f0c343e12da5cee31181a77d28715b3c0
Katharine Amaral <katharineamaral@alu.ufc.br>
2016-02-09 22:01:13 -0200

    tentando arrumar o erro

--- src/servlet/LoginClienteServlet.java
+++ src/servlet/LoginClienteServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.Cliente;
 
 /**
  * Servlet implementation class LoginClienteServlet
@@ -27,16 +30,25 @@ public class LoginClienteServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+
+
+	@SuppressWarnings("deprecation")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		
 		String login_form = request.getParameter("emailc"); // Pega o Login vindo do formulário
 		String senha_form = request.getParameter("senhac"); //Pega a senha vinda do formulário
 		if(login_form.equals(login) && senha_form.equals(senha)){ //Caso login e senha estejam corretos...
-			out.println("Logado com sucesso."); //Mostra na tela que foi logado com sucesso
+			System.out.println("Logado com sucesso."); //Mostra na tela que foi logado com sucesso
+			Cliente c = new Cliente();
+			c.setLogin_cliente(login_form);
+			c.setSenha_cliente(senha_form);
 			session.putValue("loginUsuario", login); //Grava a session com o Login
 			session.putValue("senhaUsuario", senha); //Grava a session com a Senha
+			session.setAttribute("autorizado", c);
 		}
 		else{ //Se estiverem incorretos...
-			out.println("Login ou senha inválidos."); //Exibe na tela e pede para voltar
+			System.out.println("Login ou senha inválidos."); //Exibe na tela e pede para voltar
 		}
 
 	}
